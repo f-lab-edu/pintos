24cc0117a257290201017f0d84cae2624b31759a
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-02-24 11:28:26 +0900

    Update syscall.c
    implement write()

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -12,9 +12,19 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void
-syscall_handler (struct intr_frame *f UNUSED) 
+
+int write(int fd, void *buffer, int size) {
+  putbuf(buffer, size);
+  return size;
+}
+
+static void syscall_handler(struct intr_frame* f)
 {
-  printf ("system call!\n");
-  thread_exit ();
+  int *current_esp = f->esp;
+	switch (current_esp[0])
+	{
+		case SYS_WRITE: write(current_esp[1], current_esp[2], current_esp[3]); break;
+		default: printf("Default %d\n",*current_esp);
+	}
+
 }
