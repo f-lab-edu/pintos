4f6da1ba7a6a917b224016d5ae9622ef057600e2
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-02-24 18:39:44 +0900

    Update syscall.c
    implement basic system calls

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -20,12 +20,19 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
+bool is_valid_address(void *ptr) {
+  if(ptr != NULL && is_user_vaddr(ptr) == true && pagedir_get_page(thread_current()->pagedir, ptr) != NULL)
+    return true;
+  return false;
+}
 
 void halt(void) {
   shutdown_power_off();
 }
 
 void exit(int status) {
+  if(status < 0)
+    status = -1;
   struct thread *current_thread = thread_current();
   printf("%s: exit(%d)\n", current_thread->name, status);
   current_thread->exit_status = status;
@@ -34,6 +41,8 @@ void exit(int status) {
 }
 
 int exec(const char *cmd_line) {
+  if(is_valid_address(cmd_line) == false)
+    return -1;
   return process_execute(cmd_line);
 }
 
@@ -42,9 +51,8 @@ int wait(int pid) {
 }
 
 bool create(const char *file, unsigned initial_size) {
-  if(file == NULL)
-    return false;
-
+  if(is_valid_address(file) == false)
+    exit(-1);
   return filesys_create(file, initial_size);
 }
 
@@ -53,18 +61,16 @@ bool remove(const char *file) {
 }
 
 int open(const char *file) {
-  if(file == NULL)
-    return -1;
+  if(is_valid_address(file) == false)
+    exit(-1);
 
   struct file *current_file = filesys_open(file);
   if(current_file == NULL)
     return -1;
 
   struct thread *current_thread = thread_current();
-  if(current_thread->next_fd == -1) {
-    printf("out of fd table\n");
-    return -1;
-  }
+  if(current_thread->next_fd == -1)
+    exit(-1);
 
   current_thread->fd_table[current_thread->next_fd] = current_file;
   int current_fd = current_thread->next_fd;
@@ -83,49 +89,53 @@ int open(const char *file) {
 }
 
 int filesize(int fd) {
+  if(fd < 0 || fd >= FD_TABLE_MAX_SLOT || thread_current()->fd_table[fd] == NULL)
+    exit(-1);
   return file_length(thread_current()->fd_table[fd]);
 }
 
 int read(int fd, void *buffer, unsigned size) {
+  if(fd < 0 || fd >= FD_TABLE_MAX_SLOT || thread_current()->fd_table[fd] == NULL || is_valid_address(buffer) == false || size < 0)
+    exit(-1);
+
   if(fd == STDIN_FILENO) {
     input_getc();
     return -1;    // todo
   }
-  else {
-    if(thread_current()->fd_table[fd] == NULL)
-      return -1;
+  else
     return file_read(thread_current()->fd_table[fd], buffer, size);
-  }
 }
 
 int write(int fd, const void *buffer, unsigned size) {
+  if(is_valid_address(buffer) == false || size < 0)
+    exit(-1);
+
   if(fd == STDOUT_FILENO)
     putbuf(buffer, size);
   else {
-    if(thread_current()->fd_table[fd] == NULL)
-      return -1;
+    if(fd < 0 || fd >= FD_TABLE_MAX_SLOT || thread_current()->fd_table[fd] == NULL)
+      exit(-1);
     return file_write(thread_current()->fd_table[fd], buffer, size);
   }
   return size;
 }
 
 void seek(int fd, unsigned poisiton) {
-  if(thread_current()->fd_table[fd] == NULL)
-    return;
+  if(fd < 0 || fd >= FD_TABLE_MAX_SLOT || thread_current()->fd_table[fd] == NULL)
+    exit(-1);
   file_seek(thread_current()->fd_table[fd], poisiton);
 }
 unsigned tell(int fd) {
-  if(thread_current()->fd_table[fd] == NULL)
-    return;
+  if(fd < 0 || fd >= FD_TABLE_MAX_SLOT || thread_current()->fd_table[fd] == NULL)
+    exit(-1);
   return file_tell(thread_current()->fd_table[fd]);
 }
 void close(int fd) {
   struct thread *current_thread = thread_current();
-  if(current_thread->fd_table[fd] == NULL)
-    return;
+  if(fd < 0 || fd >= FD_TABLE_MAX_SLOT || current_thread->fd_table[fd] == NULL)
+    exit(-1);
 
   file_close(current_thread->fd_table[fd]);
-
   current_thread->fd_table[fd] = NULL;
   current_thread->next_fd = fd;
 }
