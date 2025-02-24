6aeb7f34ca2ab41370d4bb9572ec7342f876c365
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-02-24 12:33:15 +0900

    Update syscall.c
    set basic skeleton

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -27,11 +27,48 @@ void exit(int status) {
   thread_exit();
 }
 
-int write(int fd, void *buffer, int size) {
+pid_t exec(const char *cmd_line) {
+  ;
+}
+
+int wait(pid_t pid) {
+  ;
+}
+
+bool create(const char *file, unsigned initial_size) {
+  ;
+}
+
+bool remove(const char *file) {
+  ;
+}
+
+int open(const char *file) {
+  ;
+}
+
+int filesize(int fd) {
+  ;
+}
+
+int read(int fd, void *buffer, unsigned size) {
+  ;
+}
+
+int write(int fd, const void *buffer, unsigned size) {
   putbuf(buffer, size);
   return size;
 }
 
+void seek(int fd, unsigned poisiton) {
+  ;
+}
+unsigned tell(int fd) {
+  ;
+}
+void close(int fd) {
+  ;
+}
 
 
 static void syscall_handler(struct intr_frame* f)
@@ -42,12 +79,22 @@ static void syscall_handler(struct intr_frame* f)
   if(is_user_vaddr(current_esp) == false)
     return; 
 
-	switch (current_esp[0])
-	{
+  switch (current_esp[0])
+  {
     case SYS_HALT: halt(); break;
     case SYS_EXIT: exit(current_esp[1]); break;
-		case SYS_WRITE: f->eax = write(current_esp[1], current_esp[2], current_esp[3]); break;
-		default: printf("Default %d\n",*current_esp);
-	}
+    case SYS_EXEC: f->eax = exec(current_esp[1]); break;
+    case SYS_WAIT: f->eax = wait(current_esp[1]); break;
+    case SYS_CREATE: f->eax = create(current_esp[1], current_esp[2]); break;
+    case SYS_REMOVE: f->eax = remove(current_esp[1]); break;
+    case SYS_OPEN: f->eax = open(current_esp[1]); break;
+    case SYS_FILESIZE: f->eax = filesize(current_esp[1]); break;
+    case SYS_READ: f->eax = read(current_esp[1], current_esp[2], current_esp[3]); break;
+    case SYS_WRITE: f->eax = write(current_esp[1], current_esp[2], current_esp[3]); break;
+    case SYS_SEEK: seek(current_esp[1], current_esp[2]); break;
+    case SYS_TELL: f->eax = tell(current_esp[1]); break;
+    case SYS_CLOSE: close(current_esp[1]); break;
 
+    default: printf("Default: %d\n",current_esp[0]);
+  }
 }
